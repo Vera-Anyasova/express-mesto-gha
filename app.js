e9a15d478c0218b50523a8e9e5298a11d3a901fa
Vera Anyasova <vera.anyasova1@yandex.ru>
2023-04-25 20:04:36 +0500

    refactor: добавить файл .env

--- app.js
+++ app.js
@@ -15,10 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("DB is connected"))
   .catch((err) => console.log(err));
 
