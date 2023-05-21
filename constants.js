5684bc685016ed970c73461e37cfda5c3e4cbbc3
Vera Anyasova <vera.anyasova1@yandex.ru>
2023-05-21 19:17:32 +0500

    внести изменения в обработчик ошибок PR14

--- constants.js
+++ constants.js
@@ -1,4 +1,19 @@
 const regex =
   /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*[\w.,@?^=%&:\/~+#-])*\/?/;
 
-module.exports = { regex };
+const STATUS_BAD_REQUEST = 400;
+const STATUS_UNAUTHORIZED = 401;
+const STATUS_FORBIDDEN = 403;
+const STATUS_NOT_FOUND = 404;
+const STATUS_CONFLICT = 409;
+const STATUS_INTERNAL_SERVER_ERROR = 500;
+
+module.exports = {
+  regex,
+  STATUS_BAD_REQUEST,
+  STATUS_UNAUTHORIZED,
+  STATUS_FORBIDDEN,
+  STATUS_NOT_FOUND,
+  STATUS_CONFLICT,
+  STATUS_INTERNAL_SERVER_ERROR,
+};
